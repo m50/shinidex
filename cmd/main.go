a543cc8a80c3dd75a529a9528f821394a0f8e99e
m50 <marisa@clardy.eu>
2024-04-07 21:04:47 +0200

    Move main.go to cmd folder

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/labstack/gommon/log"
 	"github.com/m50/shinidex/pkg/database"
 	"github.com/m50/shinidex/pkg/database/passwords"
 	"github.com/m50/shinidex/pkg/types"
 	"github.com/m50/shinidex/pkg/web"
 )
+
+func logger() *log.Logger {
+	logger := log.New("shinidex")
+	logger.SetHeader("[${time_rfc3339}] ${level} ${short_file}:${line}")
+	logger.SetLevel(log.DEBUG)
+	return logger
+}
 
 func main() {
 	if _, err := os.Stat("./.env"); err == nil {
@@ -18,29 +25,39 @@ func main() {
 		}
 	}
 
+	logger := logger()
+
 	db, err := database.NewFromEnv()
 	if err != nil {
-		log.Fatalf("DB failed to open: %s", err)
+		logger.Fatalf("DB failed to open: %s", err)
 		return
 	}
 	defer db.Close()
+	db.AttachLogger(logger)
 	if err = db.Migrate("./migrations"); err != nil {
-		log.Fatalf("Failed to migrate: %s", err)
+		logger.Fatalf("Failed to migrate: %s", err)
 		return
 	}
+
+	addTestUser(db, logger)
+
+	e := web.New(db, logger)
+	if err := e.Start(":1323"); err != nil {
+		logger.Fatal(err)
+	}
+}
+
+func addTestUser(db *database.Database, logger *log.Logger) {
 	p, err := passwords.HashPassword("test")
 	if err != nil {
-		log.Fatalf("failed to hash password for test user")
+		logger.Fatalf("failed to hash password for test user")
 		return
 	}
 	if _, err := db.Users().Insert(types.User{
 		Email: "test@test.com",
 		Password: p,
 	}); err != nil {
-		log.Fatalf("Failed to insert test user")
+		logger.Fatalf("Failed to insert test user")
 		return
 	}
-
-	e := web.New(db)
-	e.Logger.Fatal(e.Start(":1323"))
 }
