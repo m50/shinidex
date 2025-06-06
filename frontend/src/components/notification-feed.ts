efe045c094a93a4e2498c1133f35aebd8d4c4246
m50 <marisa@clardy.eu>
2025-06-07 01:39:53 +0200

    Make notificatio feed nicer

--- frontend/src/components/notification-feed.ts
+++ frontend/src/components/notification-feed.ts
@@ -1,28 +1,37 @@
 import { delay } from "../functions";
 
 function observe(mutationList: MutationRecord[]) {
+    const feed = document.getElementById("notification-feed")
+    const k = feed?.childNodes.length ?? 0
+
     for (const mutation of mutationList) {
         if (mutation.type !== "childList") {
-            continue;
+            continue
         }
         const notification = (mutation.addedNodes[0] as HTMLDivElement)
         if (!notification) {
-            continue;
+            continue
         }
-        delay(5000).then(async () => {
+        console.log(k)
+        if (k > 5) {
+            const firstElm = (feed?.lastElementChild as HTMLDivElement)
+            firstElm.classList.add("opacity-0")
+            firstElm.remove()
+        }
+        delay(3000).then(async () => {
             notification.classList.add("opacity-0")
-            await delay(1000);
-            notification.remove();
+            await delay(1000)
+            notification.remove()
         })
     }
 }
 
 export default () => {
-    const feed = document.getElementById("notification-feed");
+    const feed = document.getElementById("notification-feed")
     if (feed === null) {
-        console.log("#notification-feed not found");
-        return;
+        console.log("#notification-feed not found")
+        return
     }
-    const observer = new MutationObserver(observe);
-    observer.observe(feed, {childList: true});
+    const observer = new MutationObserver(observe)
+    observer.observe(feed, {childList: true})
 }
