9c5985b24c01e660bcd1aac24b3a0229e99bd81b
m50 <marisa@clardy.eu>
2025-06-09 23:04:30 +0200

    Add a basic registration form

--- pkg/web/handlers/auth/auth.go
+++ pkg/web/handlers/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -8,6 +9,7 @@ import (
 	"github.com/m50/shinidex/pkg/config"
 	"github.com/m50/shinidex/pkg/database"
 	"github.com/m50/shinidex/pkg/database/passwords"
+	"github.com/m50/shinidex/pkg/types"
 	"github.com/m50/shinidex/pkg/views"
 	smiddleware "github.com/m50/shinidex/pkg/web/middleware"
 	"github.com/m50/shinidex/pkg/web/session"
@@ -27,12 +29,55 @@ func Router(e *echo.Echo) {
 	group.POST("/logout", logout, smiddleware.AuthnMiddleware)
 }
 
+type registerFormData struct {
+	Email           string `form:"email"`
+	Password        string `form:"password"`
+	ConfirmPassword string `form:"confirm_password"`
+	Honeypot        string `form:"name"`
+}
+
 func registerForm(c echo.Context) error {
-	return views.RenderView(c, http.StatusOK, RegisterForm())
+	return views.RenderView(c, http.StatusOK, RegisterForm(registerFormData{}))
 }
 
 func register(c echo.Context) error {
-	return nil
+	var f registerFormData
+	if err := c.Bind(&f); err != nil {
+		return err
+	}
+	if f.Honeypot != "" {
+		return c.Redirect(http.StatusMovedPermanently, "/")
+	}
+	if f.Password != f.ConfirmPassword {
+		err := errors.New("password and confirm password must be the same")
+		return views.RenderView(c, http.StatusUnprocessableEntity,
+			RegisterForm(f), views.Error(err))
+	}
+
+	db := c.(database.DBContext).DB()
+	p, err := passwords.HashPassword(f.Password)
+	if err != nil {
+		return err
+	}
+	u := types.User{
+		Email:    f.Email,
+		Password: p,
+	}
+	userID, err := db.Users().Insert(u)
+	if err != nil {
+		return err
+	}
+	user, err := db.Users().FindByID(userID)
+	if err != nil {
+		return err
+	}
+
+	if err := session.New(c, user); err != nil {
+		c.Logger().Error(err)
+		return views.RenderError(c, err)
+	}
+
+	return c.Redirect(http.StatusMovedPermanently, "/")
 }
 
 func loginForm(c echo.Context) error {
